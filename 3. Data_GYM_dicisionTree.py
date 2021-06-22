f252ddeb885993c2e1aa1b1945c05e71299acf20
ryoki_tanaka <ryoki.tanaka@rakus-partners.co.jp>
2021-06-22 11:24:52 +0900

    Pythonデータの追記（完成）

--- 3. Data_GYM_dicisionTree.py
+++ 3. Data_GYM_dicisionTree.py
@@ -60,7 +60,7 @@ print(len(predict_data))
 predict_data.head()
 # %%
 # 予測する月の在籍期間の作成
-predict_data['period'] = None
+predict_data['period'] = 0
 predict_data['now_date'] = pd.to_datetime(predict_data['年月'], format = '%Y%m')
 predict_data['start_date'] = pd.to_datetime(predict_data['start_date'])
 for i in range(len(predict_data)):
@@ -73,8 +73,120 @@ predict_data.isna().sum()
 # count_1列に欠損値を含む行を削除する
 predict_data = predict_data.dropna(subset = ['count_1'])
 predict_data.isna().sum()
+# %% [markdown]
+# ## ダミー変数の処理
 # %%
+# データの絞り込み
 target_col = ['campaign_name', 'class_name', 'gender', 'count_1', 'routine_flg', 'period', 'is_deleted']
 predict_data = predict_data[target_col]
 predict_data.head()
+# %%
+# ダミー変数に変換する処理
+predict_data = pd.get_dummies(predict_data)
+predict_data.head()
+# %%
+predict_data.shape
+# %%
+# 余分な列を削除する
+del predict_data['campaign_name_通常']
+del predict_data['class_name_ナイト']
+del predict_data['gender_M']
+predict_data.shape
+# %% [markdown]
+# ## 決定木による退会予測モデル
+# %%
+from sklearn.tree import DecisionTreeClassifier
+import sklearn.model_selection
+
+# データの準備
+exit = predict_data.loc[predict_data['is_deleted'] == 1]
+conti = predict_data.loc[predict_data['is_deleted'] == 0].sample(len(exit)) # 退会者の数と合わせる処理
+
+X = pd.concat([exit, conti], ignore_index = True)
+y = X['is_deleted']
+del X['is_deleted']
+X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y)
+# %%
+# モデルの作成と学習
+model = DecisionTreeClassifier(random_state = 0)
+model.fit(X_train, y_train)
+y_test_pred = model.predict(X_test)
+y_test_pred
+# %%
+# 予測と正解の差異を比較する表
+results_test = pd.DataFrame({'y_test':y_test, 'y_pred':y_test_pred})
+results_test.head()
+# %%
+# 正解率を関数を用いずに算出する
+correct = len(results_test.loc[results_test['y_test'] == results_test['y_pred']])
+data_count = len(results_test)
+score_test = correct / data_count
+score_test
+# %%
+# 関数による正解率の評価 => 過学習を起こしている
+print(f'訓練用データでの正解率{model.score(X_train, y_train)}')
+print(f'評価用データでの正解率{model.score(X_test, y_test)}')
+# %%
+# モデルの修正
+X = pd.concat([exit, conti], ignore_index = True)
+y = X['is_deleted']
+del X['is_deleted']
+X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y)
+
+# モデルの構築と学習
+model_2 = DecisionTreeClassifier(random_state = 0, max_depth = 5) # 深さを指定
+model_2.fit(X_train, y_train)
+# %%
+# 改良したモデルで再評価 => 訓練と評価で同程度の精度が認められた
+print(f'訓練用データでの正解率②{model.score(X_train, y_train)}')
+print(f'評価用データでの正解率②{model.score(X_test, y_test)}')
+# %% [markdown]
+# ## 変数の寄与を確認
+# %%
+importance = pd.DataFrame({'feature_names': X.columns, 'coefficient': model_2.feature_importances_})
+importance
+# %% [markdown]
+# ## 決定木の可視化
+# %%
+from sklearn import tree
+import matplotlib.pyplot as plt
+tree.plot_tree(model_2)
+plt.show()
+# %% [markdown]
+# ## 退会の予測
+# %%
+count_1 = 3
+routine_flg = 1
+period = 10
+campaign_name = "入会費無料"
+class_name = "オールタイム"
+gender = "M"
+# %%
+if campaign_name == "入会費半額":
+    campaign_name_list = [1, 0]
+elif campaign_name == "入会費無料":
+    campaign_name_list = [0, 1]
+elif campaign_name == "通常":
+    campaign_name_list = [0, 0]
+if class_name == "オールタイム":
+    class_name_list = [1, 0]
+elif class_name == "デイタイム":
+    class_name_list = [0, 1]
+elif class_name == "ナイト":
+    class_name_list = [0, 0]
+if gender == "F":
+    gender_list = [1]
+elif gender == "M":
+    gender_list = [0]
+input_data = [count_1, routine_flg, period]
+input_data.extend(campaign_name_list)
+input_data.extend(class_name_list)
+input_data.extend(gender_list)
+# %% [markdown]
+# ## 上記で設定した顧客がいた場合の退会確率
+# - 退会 or not => 退会
+# - その確率：98.57%
+# %%
+print(model_2.predict([input_data]))
+print(model_2.predict_proba([input_data]))
 # %%
