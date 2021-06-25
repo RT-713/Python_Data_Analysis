d19794985ac623ffb5bcef3f661bb29f3cf67372
ryoki_tanaka <ryoki.tanaka@rakus-partners.co.jp>
2021-06-25 11:10:49 +0900

    ロジスティクスネットワークの設計と最適化を実装

--- 5.  Logistics _network_ optimization.py
+++ 5.  Logistics _network_ optimization.py
@@ -182,4 +182,49 @@ def condition_stock(df_plan, df_material, df_stock):
     return flag
 
 print(f'制約条件の計算結果：{condition_stock(df_plan_sol,df_material,df_stock)}')
+# %% [markdown]
+# ## ロジスティクスネットワークの設計
 # %%
+製品 = list('AB')
+需要地 = list('PQ')
+工場 = list('XY')
+レーン = (2,2)
+
+# 輸送費表 #
+tbdi = pd.DataFrame(((j,k) for j in 需要地 for k in 工場), columns=['需要地','工場'])
+tbdi['輸送費'] = [1,2,3,1]
+print(tbdi)
+
+# 需要表 #
+tbde = pd.DataFrame(((j,i) for j in 需要地 for i in 製品), columns=['需要地','製品'])
+tbde['需要'] = [10,10,20,20]
+print(tbde)
+
+# 生産表 #
+tbfa = pd.DataFrame(((k,l,i,0,np.inf) for k,nl in zip (工場,レーン) for l in range(nl) for i in 製品), 
+                    columns=['工場','レーン','製品','下限','上限'])
+tbfa['生産費'] = [1,np.nan,np.nan,1,3,np.nan,5,3]
+tbfa.dropna(inplace=True)
+tbfa.loc[4,'上限']=10
+print(tbfa)
+
+from ortoolpy import logistics_network
+_, tbdi2, _ = logistics_network(tbde,tbdi,tbfa)
+print(tbfa)
+print(tbdi2)
+# %% [markdown]
+# ## 輸送コストを計算
+# %%
+print(tbdi2)
+trans_cost = 0
+for i in range(len(tbdi2.index)):
+    trans_cost += tbdi2['輸送費'].iloc[i]*tbdi2['ValX'].iloc[i]
+print(f'総輸送コスト：{trans_cost}')
+# %% [markdown]
+# ## 生産コストを計算
+# %%
+print(tbfa)
+product_cost = 0
+for i in range(len(tbfa.index)):
+    product_cost += tbfa['生産費'].iloc[i]*tbfa['ValY'].iloc[i]
+print(f'総生産コスト：{product_cost}')
