4d964dfe36f9b2a9c5f1a843a4dfbd144d0864e6
ryoki_tanaka <ryoki.tanaka@rakus-partners.co.jp>
2021-06-29 17:49:38 +0900

    会員数の時系列変化を可視化するコードを実装

--- 6. Mathematical_Simulation.py
+++ 6. Mathematical_Simulation.py
@@ -101,7 +101,86 @@ for i in range(len(list_timeSeries)):
 
 plt.plot(list_timeSeries_num)
 plt.show()
-
+# %% [markdown]
+# ## 時系列変化（会員数）
 # %%
-df_links[node_name].iloc[15]
+def simulate_population(num, list_active, percent_percolation, percent_disapparence,df_links):
+    # 拡散のパターン
+    for i in range(num):
+        if list_active[i]==1:
+            for j in range(num):
+                if df_links.iloc[i][j]==1:
+                    if determine_link(percent_percolation)==1:
+                        list_active[j] = 1
+    # 消滅のパターン
+    for i in range(num):
+        if determine_link(percent_disapparence)==1:
+            list_active[i] = 0
+    return list_active
+# %%
+percent_percolation = 0.1
+percent_disapparence = 0.05
+T_NUM = 100
+NUM = len(df_links.index)
+list_active = np.zeros(NUM)
+list_active[0] = 1
+
+list_timeSeries = []
+for t in range(T_NUM):
+    list_active = simulate_population(NUM, list_active, percent_percolation, percent_disapparence,df_links)
+    list_timeSeries.append(list_active.copy())
+# %%
+# 時系列グラフ
+list_timeSeries_num = []
+for i in range(len(list_timeSeries)):
+    list_timeSeries_num.append(sum(list_timeSeries[i]))
+
+plt.plot(list_timeSeries_num)
+plt.show()
+# %%
+# 利用者が減少していく過程の確認
+percent_disapparence = 0.3
+list_active = np.zeros(NUM)
+list_active[0] = 1
+list_timeSeries = []
+for t in range(T_NUM):
+    list_active = simulate_population(NUM, list_active, percent_percolation, percent_disapparence,df_links)
+    list_timeSeries.append(list_active.copy())
+
+# 時系列グラフ
+list_timeSeries_num = []
+for i in range(len(list_timeSeries)):
+    list_timeSeries_num.append(sum(list_timeSeries[i]))
+
+plt.plot(list_timeSeries_num)
+plt.show()
+# %%
+# 相図計算
+print("相図計算開始")
+T_NUM = 100
+NUM_PhaseDiagram = 20
+phaseDiagram = np.zeros((NUM_PhaseDiagram,NUM_PhaseDiagram))
+for i_p in range(NUM_PhaseDiagram):
+    for i_d in range(NUM_PhaseDiagram):
+        percent_percolation = 0.05*i_p
+        percent_disapparence = 0.05*i_d
+        list_active = np.zeros(NUM)
+        list_active[0] = 1
+        for t in range(T_NUM):
+            list_active = simulate_population(NUM, list_active, percent_percolation, percent_disapparence,df_links)
+        phaseDiagram[i_p][i_d] = sum(list_active)
+print(phaseDiagram)
+
+# 図示
+plt.matshow(phaseDiagram)
+plt.colorbar(shrink=0.8)
+plt.xlabel('percent_disapparence')
+plt.ylabel('percent_percolation')
+plt.xticks(np.arange(0.0, 20.0,5), np.arange(0.0, 1.0, 0.25))
+plt.yticks(np.arange(0.0, 20.0,5), np.arange(0.0, 1.0, 0.25))
+plt.tick_params(bottom=False,
+                left=False,
+                right=False,
+                top=False)
+plt.show()
 # %%
